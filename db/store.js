7d61232f90fb31258e2912db2b65a678346176db
Rene Ortega <reneortega2468@gmail.com>
2022-04-29 15:56:07 -0400

    not done

--- db/store.js
+++ db/store.js
@@ -1,50 +1,51 @@
-// TODO Require thr util and fs needed
-const util = require('util');
-const fs = require('fs');
+// // TODO Require thr util and fs needed
+// const util = require('util');
+// const fs = require('fs');
 
-// const uuidv1 = require('uuid').v1
+// // const uuidv1 = require('uuid').v1
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-class Store {
-  read(){
-    return readFileAsync ('db.json', 'utf8');
+// const readFileAsync = util.promisify(fs.readFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
+// class Store {
+//   read(){
+//     return readFileAsync ('db.json', 'utf8');
 
-  }
+//   }
 
 
-  write(note){
-    // readable
-    return writeFileAsync('db.json', JSON.stringify(note));
-  }
+//   write(note){
+//     // readable
+//     return writeFileAsync('db.json', JSON.stringify(note));
+//   }
 
-  // get the notes 
-  // add the notes to the notes
-  // can add delete 
-  getNotes() {  
-    return this.read().then((notes) => {
-      let createdNote; 
-        try {createdNote = [].concat(JSON.parse(notes))} 
+//   // get the notes 
+//   // add the notes to the notes
+//   // can add delete 
+//   getNotes() {  
+//     return this.read().then((notes) => {
+      
+//       let createdNote; 
+//         try {createdNote = [].concat(JSON.parse(notes))} 
           
-      catch(err) {
-        createdNote = [];
-      }
-      return createdNote;
-    });
-  }
+//       catch(err) {
+//         createdNote = [];
+//       }
+//       return createdNote;
+//     });
+//   }
 
-  postNotes(note) {
-    const {title, text} = note;
-    const newNote = {title, text};
-      // write all updated notes and return the new notes
-    return this.getNotes()
-      .then((notes) => [...notes, newNote])
-        .then((updatedNotes) => this.write(updatedNotes))
-          .then(() => newNote);
+//   postNotes(note) {
+//     const {title, text} = note;
+//     const newNote = {title, text};
+//       // write all updated notes and return the new notes
+//     return this.getNotes()
+//       .then((notes) => [...notes, newNote])
+//         .then((updatedNotes) => this.write(updatedNotes))
+//           .then(() => newNote);
     
-  }
-  // filter out of array of notes and delete specific notes
+//   }
+//   // filter out of array of notes and delete specific notes
   
   
-}
-module.exports = new Store();
+// }
+// module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,70 @@
 // TODO Require the router and db  items needed 
 const router = require('express').Router();
-const store = require('../db/store');
-// const uuid = require('../helpers/uuid');
-// const { readFiles  , readAndAppend, writeFile } = require('../helpers/fsUtils')
+// const store = require('../db/store');
+
+const { readFromFile, readAndAppend, writeTofile } = require('../helpers/fsUtils')
+const uuid = require('../helpers/uuid');
+
 
 router.get('/notes', (req, res) => {
-  store
-    .getNotes() 
-    .then ((notes) => {
-      return res.json(notes);
-    }) 
-    .catch (err => {
-      res.status(500).json(err);
-
-    })
+  readFromFile("/db/db.json").then((data) => 
+  res.json(JSON.parse(data)));
 });
 
-router.post ('/notes', (req, res) => {
-  store
-    .postNotes(req.body)
-    .then((note) => {
-      res.json(note);
-    })
-    .catch (err => {
-      res.status(500).json(err);
-    })
+// posting
+router.post('/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+    if (req.body) {
+      const newNote = {
+        title,
+          text,
+            id: uuid(),
+      };
+      readAndAppend(newNote, '../db/db.json' );
+        res.json('Your note was added!')
+
+    } else {
+      res.err('Something is wrong....')
+    }
 });
+
+
+// delete logic
+router.get('/notes/:id', (req, res) =>{
+  const idNote = req.params.id;
+    readFromFile('../db/db.json').then((data) =>{
+      return JSON.parse(data);
+    })
+
+    .then((data) =>{
+      writeTofile('../db/db.json', data.filter((note) => note.id !== idNote));
+        res.json('Deleted Note!')
+    });
+});
+
+
+
+
+// router.get('/notes', (req, res) => {
+//   console.log('getting all notes')
+//   readFiles('../helpers/fsUtils.js')
+//     .then((err, notes) => {
+//       res.json(notes)
+//     })
+//     });
+
+// router.post ('/notes', (req, res) => {
+//   store
+//     .postNotes(req.body)
+//     .then((note) => {
+//       res.json(note);
+//     })
+//     .catch (err => {
+//       res.status(500).json(err);
+//     })
+// });
 
 
 module.exports = router; 
